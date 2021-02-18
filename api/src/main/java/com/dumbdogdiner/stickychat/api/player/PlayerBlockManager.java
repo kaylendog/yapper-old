156cd4c806f7e90264a46526abd3c892fced26e6
SkyezerFox <actuallyori@gmail.com>
2021-02-18 17:39:14 +0000

    v4 :sparkles: implementation of SkPlayerBlockManager

--- api/src/main/java/com/dumbdogdiner/stickychat/api/player/PlayerBlockManager.java
+++ api/src/main/java/com/dumbdogdiner/stickychat/api/player/PlayerBlockManager.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -13,12 +14,82 @@ import java.util.UUID;
  */
 public interface PlayerBlockManager {
     /**
+     * Load block information for the target player.
+     * @param player The target player
+     * @return True if the action was successful
+     */
+    @NotNull Boolean loadBlocks(@NotNull UUID player);
+
+    /**
+     * Block the target player. Returns false if the player is already blocked.
+     *
+     * @param player The uuid of the player who is blocking the target
+     * @param target The uuid of the target to block
+     * @return True if the player was blocked, false if they were already blocked.
+     */
+    @NotNull Boolean block(@NotNull UUID player, @NotNull UUID target);
+
+    /**
+     * Block the target player. Returns false if the player is already blocked.
+     *
+     * @param player The player who is blocking the target
+     * @param target The target to block
+     * @return True if the player was blocked, false if they were already blocked.
+     */
+    default @NotNull Boolean block(@NotNull OfflinePlayer player, @NotNull OfflinePlayer target) {
+        return this.block(player.getUniqueId(), target.getUniqueId());
+    }
+
+    /**
+     * Block the target player. Returns false if the player is already blocked.
+     *
+     * @param player The uuid of the player who is blocking the target
+     * @param target The uuid of the target to block
+     * @return True if the player was blocked, false if they were already blocked.
+     */
+    default @NotNull Boolean block(@NotNull Player player, @NotNull Player target) {
+        return this.block(player.getUniqueId(), target.getUniqueId());
+    }
+
+    /**
+     * Unblock the target player.
+     *
+     * @param player The uuid of the player who is unblocking the target
+     * @param target The uuid of the target to unblock
+     * @return True if the player was unblocked, false if they were not blocked to begin with.
+     */
+    @NotNull Boolean unblock(@NotNull UUID player, @NotNull UUID target);
+
+    /**
+     * Unblock the target player.
+     *
+     * @param player The uuid of the player who is unblocking the target
+     * @param target The uuid of the target to unblock
+     * @return True if the player was unblocked, false if they were not blocked to begin with.
+     */
+    default @NotNull Boolean unblock(@NotNull OfflinePlayer player, @NotNull OfflinePlayer target) {
+        return this.unblock(player.getUniqueId(),target.getUniqueId());
+    }
+
+    /**
+     * Unblock the target player.
+     *
+     * @param player The uuid of the player who is unblocking the target
+     * @param target The uuid of the target to unblock
+     * @return True if the player was unblocked, false if they were not blocked to begin with.
+     */
+    default @NotNull Boolean unblock(@NotNull Player player, @NotNull Player target) {
+        return this.unblock(player.getUniqueId(),target.getUniqueId());
+    }
+
+    /**
      * Test if the given player has blocked the target player.
      * @param player The UUID of the player
      * @param target The UUID of the player who's blocking state is being tested
      * @return True if the player has blocked the target.
      */
-    @NotNull Boolean hasPlayerBlocked(UUID player, UUID target);
+    @NotNull Boolean hasBlocked(@NotNull UUID player, @NotNull UUID target);
+
 
     /**
      * Test if the given player has blocked the target player.
@@ -26,22 +97,23 @@ public interface PlayerBlockManager {
      * @param target The player who's blocking state is being tested
      * @return True if the player has blocked the target.
      */
-    default @NotNull Boolean hasPlayerBlocked(Player player, Player target) {
-        return this.hasPlayerBlocked(player.getUniqueId(), target.getUniqueId());
+    default @NotNull Boolean hasBlocked(@NotNull OfflinePlayer player, @NotNull OfflinePlayer target) {
+        return this.hasBlocked(player.getUniqueId(), target.getUniqueId());
     }
+
     /**
      * Test if the given player has blocked the target player.
      * @param player The player
      * @param target The player who's blocking state is being tested
      * @return True if the player has blocked the target.
      */
-    default @NotNull Boolean hasPlayerBlocked(OfflinePlayer player, OfflinePlayer target) {
-        return this.hasPlayerBlocked(player.getUniqueId(), target.getUniqueId());
+    default @NotNull Boolean hasBlocked(@NotNull Player player, @NotNull Player target) {
+        return this.hasBlocked(player.getUniqueId(), target.getUniqueId());
     }
 
     /**
      * Get all outstanding blocks.
      * @return A {@link HashMap} containing all mapped blocks.
      */
-    @NotNull HashMap<UUID, List<UUID>> getAllBlocks();
+    @NotNull HashMap<UUID, Set<UUID>> getAllBlocks();
 }
--- bukkit/src/main/kotlin/com/dumbdogdiner/stickychat/bukkit/player/SkPlayerBlockManager.kt
+++ bukkit/src/main/kotlin/com/dumbdogdiner/stickychat/bukkit/player/SkPlayerBlockManager.kt
@@ -1,4 +1,87 @@
 package com.dumbdogdiner.stickychat.bukkit.player
 
-class SkPlayerBlockManager {
+import com.dumbdogdiner.stickychat.api.player.PlayerBlockManager
+import com.dumbdogdiner.stickychat.bukkit.models.Blocks
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
+import java.util.HashMap
+import java.util.UUID
+
+/**
+ * Handles block data.
+ */
+class SkPlayerBlockManager : PlayerBlockManager {
+    private val cachedBlocks = mutableMapOf<UUID, MutableSet<UUID>>()
+
+    /**
+     * Load blocks for the target player uuid.
+     */
+    override fun loadBlocks(player: UUID): Boolean {
+        // create the cache entry
+        val blocks = cachedBlocks.getOrPut(player, { mutableSetOf() })
+        // query the database for blocks
+        transaction {
+            Blocks.select {
+                Blocks.player eq player.toString() and Blocks.active
+            }.forEach {
+                // add each block to the set
+                blocks.add(UUID.fromString(it[Blocks.target]))
+            }
+        }
+        return true
+    }
+
+    /**
+     * Adds the block to the cache and the database.
+     */
+    override fun block(player: UUID, target: UUID): Boolean {
+        // update the database - do this first in case it errors
+        transaction {
+            Blocks.insert {
+                it[Blocks.player] = player.toString()
+                it[Blocks.target] = target.toString()
+                it[active] = true
+                it[createdAt] = System.currentTimeMillis()
+            }
+        }
+        // add the player to cache - creates the set if it does not exist
+        cachedBlocks.getOrPut(player, { mutableSetOf() }).add(target)
+        return true
+    }
+
+    /**
+     * Removes a block from the cache and the database.
+     */
+    override fun unblock(player: UUID, target: UUID): Boolean {
+        // update the database - do this first in case it errors
+        transaction {
+            Blocks.update({ Blocks.player eq player.toString() }) {
+                it[active] = false
+                it[deactivatedAt] = System.currentTimeMillis()
+            }
+        }
+        // remove the player from the cache
+        cachedBlocks.getOrPut(player, { mutableSetOf() }).remove(target)
+        return true
+    }
+
+    /**
+     * Tests if the player has blocked the other.
+     */
+    override fun hasBlocked(player: UUID, target: UUID): Boolean {
+        // check if the cache initially has the player
+        if (cachedBlocks.containsKey(player)) {
+            return cachedBlocks[player]!!.contains(target)
+        }
+        // their data is not loaded - must load it!
+        this.loadBlocks(player)
+        return cachedBlocks.getOrPut(player, { mutableSetOf() }).contains(target)
+    }
+
+    override fun getAllBlocks(): HashMap<UUID, Set<UUID>> {
+        return HashMap(this.cachedBlocks.mapValues { it.value.toSet() })
+    }
 }
