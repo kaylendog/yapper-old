054e32c49ed66bde979a3cdac8d5bb8227fd07ad
SkyezerFox <actuallyori@gmail.com>
2021-02-23 22:14:41 +0000

    attempt to implement versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("net.nemerosa.versioning") version "2.8.2"
+    id("net.nemerosa.versioning") version "2.14.0"
     kotlin("jvm") version "1.4.10"
     java
 }
 
 allprojects {
     version = versioning.info.full
+    group = "com.dumbdogdiner.stickychat"
 
     repositories {
         jcenter()
         mavenCentral()
     }
-}
-
-subprojects {
-    group = "com.dumbdogdiner.stickychat"
 
     apply(plugin = "java")
     apply(plugin = "kotlin")
 
-    repositories {
-        jcenter()
-        mavenCentral()
-    }
 
     tasks.withType<JavaCompile> {
         targetCompatibility = JavaVersion.VERSION_14.toString()
