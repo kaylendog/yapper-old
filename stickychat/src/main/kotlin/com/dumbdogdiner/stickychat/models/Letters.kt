b9a392b431028bd95eac044f8f8a94fc1de9b4a5
SkyezerFox <actuallyori@gmail.com>
2021-05-21 21:16:00 +0100

    remove old plugin code

--- stickychat/src/main/kotlin/com/dumbdogdiner/stickychat/models/Letters.kt
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2020-2021 Skye Elliot. All rights reserved.
- * Licensed under the GNU General Public License v3, see LICENSE for more information...
- */
-package com.dumbdogdiner.stickychat.models
-
-object Letters
